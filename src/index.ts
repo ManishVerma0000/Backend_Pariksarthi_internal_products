c694d1ac5b3ba997349ac39464d95ccad0d6fa41
Manish Verma <manish.verma@mintworxs.co>
2025-01-12 20:10:49 +0530

    updated the index.ts file added the cutosm port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import router from "./routes";
 const app = express();
-const PORT = process.env.PORT||4000;
+const PORT =4001;
 import createConnection from "./db/conn";
 app.use(cors());
 createConnection();
