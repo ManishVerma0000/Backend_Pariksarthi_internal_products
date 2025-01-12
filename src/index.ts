88cb7d1c26d175f3bdd6ef2c490bf4b699180659
Manish Verma <manish.verma@mintworxs.co>
2025-01-12 20:07:43 +0530

    hot fix

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,7 @@ createConnection();
 app.use(express.json());
 app.use("/api", router);
 
-const IPAddress = "192.168.1.109";
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://${IPAddress}:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
