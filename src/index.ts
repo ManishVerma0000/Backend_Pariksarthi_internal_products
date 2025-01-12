470c5f1a18d6e20689e19386f42fe90913840bd8
Manish Verma <manish.verma@mintworxs.co>
2025-01-12 19:52:03 +0530

    change the port or added the port in the env

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
+require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import router from "./routes";
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT||4000;
 import createConnection from "./db/conn";
 app.use(cors());
 createConnection();
 app.use(express.json());
 app.use("/api", router);
 
-const IPAddress = "192.168.1.101";
+const IPAddress = "192.168.1.109";
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://${IPAddress}:${PORT}`);
